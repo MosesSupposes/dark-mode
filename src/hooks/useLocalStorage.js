72f58baa8ec9849196a190b6c0f9fc27359d63e7
Moses A. Samuel <mosamuel98@gmail.com>
2019-09-04 13:42:23 -0700

    fix typo

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,4 +1,4 @@
-import { UseState } from 'react'
+import { useState } from 'react'
 
 export default function useLocalStorage(key, initialValue) {
     const [storedValue, setStoredValue] = useState(() => {
